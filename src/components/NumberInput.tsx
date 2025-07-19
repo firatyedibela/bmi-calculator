cc5a81ad89174aac9b3da911be500a708a1352b5
firatyedibela <yedibelafirat0@gmail.com>
2025-07-19 19:59:58 +0300

    feat: register input to form using form context

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -1,12 +1,20 @@
+import { useFormContext } from 'react-hook-form';
+
 type NumberInputProps = {
   unit: 'cm' | 'kg' | 'ft' | 'in' | 'st' | 'lbs';
   name: string;
 };
 
 export const NumberInput = ({ unit, name }: NumberInputProps) => {
+  const { register } = useFormContext();
+
   return (
     <label className="p-6 flex gap-6 border rounded-[12px] cursor-pointer flex-1">
-      <input type="number" name={name} className="outline-none w-full" />{' '}
+      <input
+        type="number"
+        className="outline-none w-full"
+        {...register(name, { required: true })}
+      />
       <span>{unit}</span>
     </label>
   );
