d0bb6c8d045d7392d4c59cec802a1bc56449950b
firatyedibela <yedibelafirat0@gmail.com>
2025-07-20 14:50:37 +0300

    fix: decrease max ideal bmi to 24.9 from 24.99

--- src/utils/getIdealWeightRange.tsx
+++ src/utils/getIdealWeightRange.tsx
@@ -2,6 +2,6 @@ export const getIdealWeightRange = (
   heightCm: number
 ): { minWeight: number; maxWeight: number } => {
   const minWeight = 18.5 * (heightCm / 100) ** 2;
-  const maxWeight = 24.99 * (heightCm / 100) ** 2;
+  const maxWeight = 24.9 * (heightCm / 100) ** 2;
   return { minWeight, maxWeight };
 };
