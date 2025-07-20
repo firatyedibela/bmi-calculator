4f5d24a7283305cce708710cf9441c6e89f0ba28
firatyedibela <yedibelafirat0@gmail.com>
2025-07-20 14:11:31 +0300

    feat: implement RadioIcon component

---
+++ src/components/RadioIcon.tsx
@@ -0,0 +1,22 @@
+import clsx from 'clsx';
+
+type RadioIconProps = {
+  isSelected: boolean;
+};
+
+export const RadioIcon = ({ isSelected }: RadioIconProps) => {
+  const sharedStyles = 'w-[31px] h-[31px] rounded-full';
+
+  return isSelected ? (
+    <div
+      className={clsx(
+        sharedStyles,
+        'bg-blue-100 flex justify-center items-center'
+      )}
+    >
+      <div className="w-[15px] h-[15px] rounded-full bg-blue-500"></div>
+    </div>
+  ) : (
+    <div className={clsx(sharedStyles, 'border border-grey-500')}></div>
+  );
+};
