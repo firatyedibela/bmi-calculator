5f99a5f711f94750cf52687de935540c44928112
firatyedibela <yedibelafirat0@gmail.com>
2025-07-22 21:32:43 +0300

    feat: replace div with span inside label

--- src/components/RadioIcon.tsx
+++ src/components/RadioIcon.tsx
@@ -8,20 +8,20 @@ export const RadioIcon = ({ isSelected }: RadioIconProps) => {
   const sharedStyles = 'w-[31px] h-[31px] rounded-full';
 
   return isSelected ? (
-    <div
+    <span
       className={clsx(
         sharedStyles,
         'radio-icon-active bg-blue-100 flex justify-center items-center'
       )}
     >
-      <div className="w-[15px] h-[15px] rounded-full bg-blue-500"></div>
-    </div>
+      <span className="w-[15px] h-[15px] rounded-full bg-blue-500"></span>
+    </span>
   ) : (
-    <div
+    <span
       className={clsx(
         sharedStyles,
         'radio-icon-inactive border border-grey-500'
       )}
-    ></div>
+    ></span>
   );
 };
