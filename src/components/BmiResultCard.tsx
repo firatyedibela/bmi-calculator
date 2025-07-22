1afb34921947fe927a282c03a9f518286253313b
firatyedibela <yedibelafirat0@gmail.com>
2025-07-22 14:43:15 +0300

    feat: use utility functions to format bmi range

--- src/components/BmiResultCard.tsx
+++ src/components/BmiResultCard.tsx
@@ -1,4 +1,6 @@
 import { getImperialWeight } from '../utils/getImperialWeight';
+import { formatMetricWeightRange } from '../utils/formatMetricWeightRange';
+import { formatImperialWeightRange } from '../utils/formatImperialWeightRange';
 
 type BmiResultCardProps = {
   bmiValue: number | null;
@@ -29,9 +31,22 @@ export const BmiResultCard = ({
     imperialWeightRange.min = getImperialWeight(minWeight);
   }
 
+  let feedbackMessage: string = '';
+  if (bmiValue) {
+    if (bmiValue < 18.5) {
+      feedbackMessage =
+        'Your BMI is below the healthy range. You may need to gain weight for better health.';
+    } else if (bmiValue > 25.9) {
+      feedbackMessage =
+        'Your BMI is above the healthy range. Losing some weight may help improve your health.';
+    } else {
+      feedbackMessage = 'Your BMI suggests you’re a healthy weight.';
+    }
+  }
+
   return (
     <div className="result-card w-full p-8 bg-blue-500 flex flex-col gap-4 text-white m-auto">
-      {!!bmiValue ? (
+      {!!bmiValue && bmiValue > 0 && bmiValue < 1000 ? (
         <div className="flex flex-col gap-6 sm:flex-row sm:justify-between sm:items-center ">
           <div className="flex flex-col gap-2 flex-1">
             <span className="text-preset-6">Your BMI is...</span>
@@ -39,27 +54,14 @@ export const BmiResultCard = ({
               {bmiValue?.toFixed(1)}
             </span>
           </div>
-
-          {unitSystem === 'metric' ? (
-            <p className="text-preset-7 flex-1">
-              Your BMI suggests you’re a healthy weight. Your ideal weight is
-              between{' '}
-              <strong>
-                {minWeight.toFixed(1)}kgs - {maxWeight.toFixed(1)}kgs.
-              </strong>
-            </p>
-          ) : (
-            <p className="text-preset-7 flex-1">
-              Your BMI suggests you’re a healthy weight. Your ideal weight is
-              between{' '}
-              <strong>
-                {imperialWeightRange.min.weightSt}st{' '}
-                {Math.floor(imperialWeightRange.min.weightLbs)}lbs -{' '}
-                {imperialWeightRange.max.weightSt}st{' '}
-                {Math.floor(imperialWeightRange.max.weightLbs)}lbs.
-              </strong>
-            </p>
-          )}
+          <p className="text-preset-7 flex-1">
+            {feedbackMessage} Your ideal weight is between{' '}
+            <strong>
+              {unitSystem === 'metric'
+                ? formatMetricWeightRange(minWeight, maxWeight)
+                : formatImperialWeightRange(imperialWeightRange)}
+            </strong>
+          </p>
         </div>
       ) : (
         <>
